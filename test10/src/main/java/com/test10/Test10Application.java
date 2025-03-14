df6a849c7b0cf2d2557ac36ade1bdaa412822722
Arshiya-Khanam <arshiyaafkhan001@gmail.com>
2025-03-14 21:29:43 +0530

    Update Test10Application.java

--- test10/src/main/java/com/test10/Test10Application.java
+++ test10/src/main/java/com/test10/Test10Application.java
@@ -9,9 +9,9 @@ public class Test10Application {
 	public static void main(String[] args) {
                 String name="mike";
 		int x=100;
+		boolean isPresent=false;
 		int y=200;
-		double z=10.3;
-                char m='a';
+		char m='a';
                 SpringApplication.run(Test10Application.class, args);
 	}
 
